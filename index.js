e2007525c1c876829a7df22de0e34d487ef608fd
Elysiayn <wendy.lemus.wl@gmail.com>
2020-12-16 18:02:31 -0500

    included project starter code

--- index.js
+++ index.js
@@ -1,50 +1,76 @@
 // Packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-// const generatePage = require('./src/readme-template');  -might not need for this project
+const generateMarkdown = require('./utils/generateMarkdown');
 
-//Question array for user input
-const promptUser = () => {
-    return inquirer.prompt([{
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project? (Required)',
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your project title!');
-                    return false;
-                }
+// TODO: Create an array of questions for user input
+const questions = [{
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter your project title!');
+                return false;
             }
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Provide a description of the project (Required)',
-            validate: descriptionInput => {
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your project description!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'checkbox',
-            name: 'tableContents',
-            message: 'What sections would you like to include in the Table of Contents? (Check all that apply)',
-            choices: [
-                'Subtitle', 'Description', 'Table of Contents', 'Installation', 'Features', 'Usage', 'Deployed Production',
-                'License', 'Badges', 'Tests', 'Questions', 'Contribution'
-            ]
         }
-    ]);
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter your project description!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmTable',
+        message: 'Would you like to include a table of contents?',
+        default: true
+    },
+    {
+        type: 'checkbox',
+        name: 'tableContents',
+        message: 'What sections would you like to include in the Table of Contents? (Check all that apply)',
+        choices: [
+            'Subtitle', 'Description', 'Installation', 'Features', 'Usage', 'Deployed Production',
+            'License', 'Badges', 'Tests', 'Questions', 'Contribution'
+        ],
+        when: ({
+            confirmTable
+        }) => {
+            if (confirmTable) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+//use inquirer to prompt user w/ question array. Returns user response data.
+function init() {
+    return inquirer.prompt(questions)
+        .then(responseData => {
+            console.log('Done!')
+            writeToFile('README.md', generateMarkdown(responseData))
+        })
 };
 
-//function for project question prompts
-// const promptReadme = readmeData => {
+// This is how to include a key when console log
+// console.log(responseData['description']) or responseData.description  both are same
 
-//function calls 
-promptUser();
+// Function call to initialize app
+init();
