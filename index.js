9ff2a44b4f0ea54d64bb2fe249c6b0a56c74d2a2
Elysiayn <wendy.lemus.wl@gmail.com>
2020-12-16 14:45:14 -0500

    updated promptUser questions

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
+// Packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require('./src/readme-template');
+// const generatePage = require('./src/readme-template');  -might not need for this project
+
+//Question array for user input
+const promptUser = () => {
+    return inquirer.prompt([{
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'tableContents',
+            message: 'What sections would you like to include in the Table of Contents? (Check all that apply)',
+            choices: [
+                'Subtitle', 'Description', 'Table of Contents', 'Installation', 'Features', 'Usage', 'Deployed Production',
+                'License', 'Badges', 'Tests', 'Questions', 'Contribution'
+            ]
+        }
+    ]);
+};
+
+//function for project question prompts
+// const promptReadme = readmeData => {
+
+//function calls 
+promptUser();
